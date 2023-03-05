de210d12f1642aab294cbd3838e716bce44b57ed
kierzek <kierzek@student.agh.edu.pl>
2023-03-05 18:32:48 +0100

    add class

---
+++ LAB01/Inheritance/Vehicle.cs
@@ -0,0 +1,21 @@
+﻿namespace Inheritance
+{
+    class Vehicle 
+    {
+        private int _capacity;
+        private float _length;
+        private float _weight;
+
+
+        public int Capacity { get => _capacity; set => _capacity = value; }
+        public float Length { get => _length; set => _length = value; }
+        public float Weight { get => _weight; set => _weight = value; }
+
+
+        Vehicle()
+        {
+
+        }
+    }
+
+}
