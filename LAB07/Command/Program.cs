cd0cea55096f4b59f16ef41e3b5508c1ea44d09c
kierzek <kierzek@student.agh.edu.pl>
2023-04-25 23:58:25 +0200

    some improv

--- LAB07/Command/Program.cs
+++ LAB07/Command/Program.cs
@@ -2,17 +2,20 @@
 {
     internal class Program
     {
-        public static double Cash = 2000;
+        public static double Cash = 1000;
         static void Main(string[] args)
         {
+            #region Initialize Trip Scheduler
             Console.WriteLine($"You have {Cash}$");
             TripScheduler tripScheduler = new TripScheduler();
+            #endregion
 
+
+            #region Scenario 1
             Console.WriteLine("|=============== Scenario 1 ===================|");
-            // 1st Scenario
             List<IVisitTouristAttraction> directions = new List<IVisitTouristAttraction>
             {
-                new VisitFountain(), new VisitMuseum(3, Tickets.HalfPrice), new VisitGate(), new VisitBeach(2, IceCreams.Strawberry)
+                new VisitMuseum(3, Tickets.HalfPrice), new VisitFountain(), new VisitBeach(2, IceCreams.Strawberry), new VisitGate()
             };
 
             foreach( var direction in directions )
@@ -21,9 +24,12 @@
                 tripScheduler.Trip();
                 Console.WriteLine($"You have {Cash}$");
             }
+            #endregion
 
+
+            #region Scenario 2
             Console.WriteLine("|=============== Scenario 2 ===================|");
-            // 2nd Scenario
+            Cash += 500;
             directions = new List<IVisitTouristAttraction>
             {
                 new VisitFountain(3), new VisitMuseum(5, Tickets.Normal), new VisitGate(1), new VisitBeach(3, IceCreams.Caramel)
@@ -35,12 +41,15 @@
                 tripScheduler.Trip();
                 Console.WriteLine($"You have {Cash}$");
             }
+            #endregion
 
+
+            #region Scenario 3
             Console.WriteLine("|=============== Scenario 3 ===================|");
-            // 3rd Scenario
+            Cash += 500;
             directions = new List<IVisitTouristAttraction>
             {
-                new VisitFountain(5), new VisitMuseum(4, Tickets.Student), new VisitGate(4), new VisitBeach(1)
+                 new VisitBeach(1),  new VisitGate(4), new VisitMuseum(4, Tickets.Student), new VisitFountain(5)
             };
 
             foreach (var direction in directions)
@@ -49,6 +58,7 @@
                 tripScheduler.Trip();
                 Console.WriteLine($"You have {Cash}$");
             }
+            #endregion
         }
     }
 }
